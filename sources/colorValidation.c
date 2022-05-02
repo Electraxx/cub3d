9bbfd4e631b3afa205a749f6e5f6b9b4da3aa297
victor vermot <victorvermot@gmail.com>
2022-05-02 11:37:37 +0200

    color refactoring

--- sources/colorValidation.c
+++ sources/colorValidation.c
@@ -1,13 +1,28 @@
 #include "../headers/cub3d.h"
 
+static error_type	ft_check_color(int *colors, char **c_strs)
+{
+	int	i;
+
+	i = 0;
+	while (i < 3)
+	{
+		colors[i] = ft_atoi(c_strs[i]);
+		ft_is_str_full_num(c_strs[i]);
+		if ((colors[i] > 255 || colors[i] < 0)
+			|| !ft_is_str_full_num(c_strs[i]))
+			return (COLOR_ERROR);
+		i++;
+	}
+	return (CHECK_OK);
+}
+
 error_type	is_color_valid(char *color_line)
 {
 	char	*color;
 	char	**c_strs;
 	int		*colors;
-	int		i;
 
-	i = 0;
 	color = color_line + 2;
 	c_strs = ft_split(color, ',');
 	if (ft_2dim_char_sz(c_strs) != 3)
@@ -16,17 +31,11 @@ error_type	is_color_valid(char *color_line)
 		return (COLOR_ERROR);
 	}
 	colors = malloc(sizeof(int) * 3);
-	while (i < 3)
+	if (ft_check_color(colors, c_strs) == COLOR_ERROR)
 	{
-		colors[i] = ft_atoi(c_strs[i]);
-		ft_is_str_full_num(c_strs[i]);
-		if ((colors[i] > 255 || colors[i] < 0) || !ft_is_str_full_num(c_strs[i]))
-		{
-			free(colors);
-			ft_free_2d_str(c_strs);
-			return (COLOR_ERROR);
-		}
-		i++;
+		free(colors);
+		ft_free_2d_str(c_strs);
+		return (COLOR_ERROR);
 	}
 	free(colors);
 	ft_free_2d_str(c_strs);
